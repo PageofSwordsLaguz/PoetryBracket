31e901a662beacabf9b0d3e6838452f7218eeb1a
PageofSwordsLaguz <rebecca.a.powell@usm.edu>
2025-03-26 15:08:43 -0500

    Deploy latest build to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Votingpage from "./components/Votingpage.jsx";
-import WinnerPage from "./components/WinnerPage.jsx"; 
+import WinnerPage from "./components/WinnerPage.jsx";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/PoetryBracket">
       <div className="p-4 text-center">
         <h1 className="text-3xl font-bold mb-4">Poetry Bracket Voting App</h1>
         <Routes>
